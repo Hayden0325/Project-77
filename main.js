47a2c3072d5d3665891ba0e864401b1cad8d6c8e
Hayden0325 <110002698+Hayden0325@users.noreply.github.com>
2022-07-25 18:47:33 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+console.log("my first project");
+
+var first_name = Hayden;
+var last_name = Wong;
+
+var full_name = first_name.concat(last_name);
+console.log(full_name);
+
+var a = 10;
+var b = 5;
+
+var add = a + b;
+console.log(add);
+var sub = a - b;
+console.log(sub);
+var div = a / b;
+console.log(div);
+var mul = a * b;
+console.log(mul);
